6ef1cae4a04571b63b95fccd459817236aca7b9d
Bùi Minh <volmicrolab@gmail.com>
2023-02-12 17:23:47 +0000

    deploy config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,5 +50,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '0.0.0.0', port: 3000 }
 end
